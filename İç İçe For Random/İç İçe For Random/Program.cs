3e604d9634afbb5d6c19e46ac9a0f1139cae454e
cem eren yardımcı <er7tr1245@gmail.com>
2025-09-11 15:01:06 +0300

    Csharp-With-Random-Number-For

--- İç İçe For Random/İç İçe For Random/Program.cs
+++ İç İçe For Random/İç İçe For Random/Program.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace İç_İçe_For_Random
 {
     class Program
-    {
-        
+    {    
         static void Main(string[] args)
         {
             Random rs = new Random();
